06127c826705e5ef92ee26396f05719e2a0672cd
第十六夜月 <mashanghai88@gmail.com>
2026-07-23 16:27:46 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -60,7 +60,7 @@ UPDATE_PACKAGE "theme-fluent" "LazuliKao/luci-theme-fluent" "main"
 # 自定义
 UPDATE_PACKAGE "substore" "XiaoHaiSly/OpenWrt-SubStore" "main"
 UPDATE_PACKAGE "miaomiaowu" "XiaoHaiSly/OpenWrt-MMW" "main"
-UPDATE_PACKAGE "adguardhome" "XiaoHaiSly/luci-app-adguardhome" "main"
+UPDATE_PACKAGE "luci-app-adguardhome" "XiaoHaiSly/luci-app-adguardhome" "main"
 UPDATE_PACKAGE "lucky" "XiaoHaiSly/luci-app-lucky" "main"
 
 UPDATE_PACKAGE "momo" "nikkinikki-org/OpenWrt-momo" "main"
